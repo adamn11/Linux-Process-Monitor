cd503f1c86917cad2e036018fe618d04acb461b0
adamn11 <adamnguyen73@gmail.com>
2019-02-12 14:42:24 -0600

    Added history for versions. Updated version to correct and current num

--- version.py
+++ version.py
@@ -1,2 +1,19 @@
-__version__ = "v1.1.8"
+__version__ = "v1.2.7"
+
+'''
+v1.0.0     12/04/18 - Working version of program 
+
+v1.0.1     12/06/18 - Moved validation methods to separate file
+
+v1.0.2     02/08/19 - Check parse data for newline, which caused the program to crash
+
+v1.1.7     02/08/19 - !Renamed Main.py to Core.py
+                    - Fixed validations
+                    - Added method in Process.py to get new PID
+                    - Added git ignore file
+                    - Reduced code for infinite loop inside mem_monitor()
+                    - Fixed stop point bug
+
+v1.2.7     02/11/19 - !Moved plotting methods to separate file
+'''
 
