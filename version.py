6ba69cbc63cfeca81cf895f394e3f37679e19e12
adamn11 <adamnguyen73@gmail.com>
2019-02-21 09:42:50 -0600

    PEP8 version.py

--- version.py
+++ version.py
@@ -5,7 +5,8 @@ v1.0.0     12/04/18 - Working version of program
 
 v1.0.1     12/06/18 - Moved validation methods to separate file
 
-v1.0.2     02/08/19 - Check parse data for newline, which caused the program to crash
+v1.0.2     02/08/19 - Check parse data for newline, which caused the program to
+                        crash
 
 v1.1.7     02/08/19 - !Renamed Main.py to Core.py
                     - Fixed validations
@@ -16,8 +17,9 @@ v1.1.7     02/08/19 - !Renamed Main.py to Core.py
 
 v1.2.7     02/11/19 - !Moved plotting methods to separate file
 
-v1.3.7     02/12/19 - !Added a check to see if dependencies are installed. Will not
-                      plot if dependences are not installed.
+v1.3.7     02/12/19 - !Added a check to see if dependencies are installed. Will
+                        not
+                      plot if dependencies are not installed.
 
 v1.3.8     02/13/19 - Fixed importing modules issue
 
@@ -25,11 +27,11 @@ v1.3.9     02/14/19 - PEP8 code base
 
 v1.3.10    02/14/19 - Fixed issue from new line added into command line 
 
-v1.3.11    02/20/19 - Use getters in Process.py instead of pulling variable directly
+v1.3.11    02/20/19 - Use getters in Process.py instead of pulling variable 
+                        directly
 
 v1.3.12    02/20/19 - Fixed '/bin/sh: 1: Syntax error: "(" unexpected' error
 
 v1.3.13    02/21/19 - Cleaned code
 
 '''
-
